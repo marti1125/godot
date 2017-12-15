4eeb3d66342445d0dfd560df82194df96f436c0a
Willy Aguirre <marti1125@gmail.com>
2017-12-15 01:06:52 -0500

    improve Camera.gd

--- gdquest-transform-the-canvas/Camera.gd
+++ gdquest-transform-the-canvas/Camera.gd
@@ -1,21 +1,14 @@
 extends Node2D
 
-onready var screen_size = Vector2(Globals.get("display/width"), Globals.get("display/height"))
+onready var screen_size = OS.get_window_size()
 onready var player = get_node("Player")
-onready var last_player_pos = player.get_pos()
 
 func _ready():
-	var canvas_transform = get_viewport().get_canvas_transform()
-	canvas_transform[2] = last_player_pos - screen_size / 2
-	get_viewport().set_canvas_transform(canvas_transform)
+	update_camera()
 
 func update_camera():
-	var player_offset = last_player_pos - player.get_pos()
-	last_player_pos = player.get_pos()
-	
 	var canvas_transform = get_viewport().get_canvas_transform()
-	canvas_transform[2] += player_offset 
+	canvas_transform[2] = - player.get_pos() + screen_size / 2
 	get_viewport().set_canvas_transform(canvas_transform)
-	
-	return player_offset
+
 	
