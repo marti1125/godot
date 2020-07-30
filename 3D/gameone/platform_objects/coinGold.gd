a2c228be83463012f86ae43d498bba4d89e476a8
Willy Aguirre <marti1125@gmail.com>
2020-07-29 21:58:22 -0300

    added new examples
    Godot 3D examples

---
+++ 3D/gameone/platform_objects/coinGold.gd
@@ -0,0 +1,4 @@
+extends Area
+
+func _on_coinGold_body_entered(body):
+	queue_free()
---
+++ 3D/gameone/platform_objects/spikes.gd
@@ -0,0 +1,5 @@
+extends Area
+
+func _on_spikes_body_entered(body):
+	if body.has_method("take_damage"):
+		body.take_damage()
