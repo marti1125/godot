7686186a3745bd0987f7cf5436ad37ef9e4a7ef6
Willy Aguirre <marti1125@gmail.com>
2020-08-29 18:16:37 -0300

    added tank game

---
+++ 3D/TankGame/MiniTank.gd
@@ -0,0 +1,25 @@
+extends KinematicBody
+
+export var gravity = Vector3.DOWN * 10
+export var speed = 4
+export var rot_speed = 0.85
+
+var velocity = Vector3.ZERO
+
+func _physics_process(delta):
+	velocity += gravity * delta
+	get_input(delta)
+	velocity = move_and_slide(velocity, Vector3.UP)
+
+func get_input(delta):
+	var vy = velocity.y
+	velocity = Vector3.ZERO
+	if Input.is_action_pressed("forward"):
+		velocity += transform.basis.z * speed
+	if Input.is_action_pressed("back"):
+		velocity += -transform.basis.z * speed
+	if Input.is_action_pressed("right"):
+		rotate_y(-rot_speed * delta)
+	if Input.is_action_pressed("left"):
+		rotate_y(rot_speed * delta)
+	velocity.y = vy
